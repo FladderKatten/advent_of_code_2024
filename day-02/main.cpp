760ffc986bc792e2422890d1f0e1fdf992c7cf85
ajo <fladderkatten@gmail.com>
2024-12-07 21:45:01 +0100

    rewrote day-02 & added part 2

--- day-02/main.cpp
+++ day-02/main.cpp
@@ -1,66 +1,60 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <sstream>
-#include <string>
-#include <algorithm>
-#include <array>
+#include "../common/util.h"
 
-
-class Report
-{
-    std::vector<int> levels;
-public:
-    Report(std::istringstream &ss) {
-        int n;
-        while (ss >> n)
-            levels.push_back(n);
-    }
-
-    bool is_safe_dec() {
-        for (int i = 1; i < levels.size(); i++) {
-            int d = levels[i-1] - levels[i];
+bool is_safe_dec(const std::vector<int>& levels) {
+    for (int i = 1; i < levels.size(); i++) {
+        int d = levels[i-1] - levels[i];
             if (d < 1 || d > 3)
                 return false;
-        }
-        return true;
     }
+    return true;
+}
 
-    bool is_safe_inc() {
-        for (int i = 1; i < levels.size(); i++) {
-            int d = levels[i] - levels[i-1];
+bool is_safe_inc(const std::vector<int>& levels) {
+    for (int i = 1; i < levels.size(); i++) {
+        int d = levels[i] - levels[i-1];
             if (d < 1 || d > 3)
                 return false;
-        }
+    }
+    return true;
+}
+
+
+bool is_safe(const std::vector<int>& report) {
+    return is_safe_dec(report)
+        || is_safe_inc(report);
+}
+
+bool is_safe_enough(const std::vector<int>& report) {
+    if (is_safe(report))
         return true;
-    }
 
-    bool is_safe() {
-        return is_safe_dec() || is_safe_inc();
-    }
-};
+    for (int i = 0; i < report.size(); i++) {
+        auto modded = report;
+        modded.erase(modded.begin() + i);
 
+        if (is_safe(modded))
+            return true;
+    }
+    return false;
+}
 
 int main() {
+    int answer1 = 0;
+    int answer2 = 0;
 
-    std::ifstream f("input.txt");
+    auto lines = read_file_lines("input.txt");
+    auto reports = std::vector<std::vector<int>>();
 
-    if (!f.is_open()) {
-        std::cout << "Error opening file" << std::endl;
-        return 0;
+    for (auto &line : lines)
+        reports.emplace_back(parse_ints(line, " "));
+
+    for (auto& report : reports) {
+        answer1 += is_safe(report);
+        answer2 += is_safe_enough(report);
     }
 
-    int safe_reports = 0;
-    for (std::string line; std::getline(f, line); ) {
-        std::istringstream ss(line);
-
-        Report rep(ss);
-
-        safe_reports += rep.is_safe();
-    }
-    f.close();
-
-    std::cout << "Answer: " << safe_reports << std::endl;
+    std::cout << "Answer Part 1: " << answer1 << std::endl;
+    std::cout << "Answer Part 2: " << answer2 << std::endl;
 
 
 }
